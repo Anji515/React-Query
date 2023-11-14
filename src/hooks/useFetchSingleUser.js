6f8e761339d586760b41bb63871e0a480fb81333
Anji@515 <anji.gd17@gmail.com>
2023-11-14 11:19:16 +0530

    Intial query data to get single user data instantly

--- src/hooks/useFetchSingleUser.js
+++ src/hooks/useFetchSingleUser.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useQuery } from "react-query";
+import {useQuery , useQueryClient  } from "react-query";
 
 const fetchRqUsers = ({queryKey}) => {
 
@@ -8,7 +8,22 @@ const fetchRqUsers = ({queryKey}) => {
 };
 
 const useFetchSingleUser = (userId) => {
-    return useQuery(['single-user', userId],fetchRqUsers)
+  
+  const queryClient=useQueryClient()
+
+    return useQuery(['single-user', userId],fetchRqUsers,{
+      initialData:()=>{
+        const user = queryClient.getQueryData('rq-users')?.data?.find(user=> user.id=== parseInt(userId))
+
+        if(user){
+          return {
+            data: user
+          }
+        }else {
+          return undefined
+        }
+      }
+    })
 }
 
 export default useFetchSingleUser
